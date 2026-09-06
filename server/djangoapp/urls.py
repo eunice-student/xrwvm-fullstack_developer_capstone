aea7b7400c23beb07592d5079265f847192f97bb
Eunice <eunicevr71@gmail.com>
2026-09-06 13:13:38 -0600

    Add registration path to Django URLs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,6 +14,9 @@ urlpatterns = [
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
 
+    #path for
+    path(route='register', view=views.register_user, name='register'),
+
     # path for dealer reviews view
 
     # path for add a review view
